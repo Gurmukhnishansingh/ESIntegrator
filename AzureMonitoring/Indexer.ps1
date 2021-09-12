6e8b37c4420b6a3bb76e44f387349aee3050b778
Gurmukhnishan Singh <nishansaiyan@gmail.com>
2021-09-12 19:36:54 +0530

    Update Indexer.ps1

--- AzureMonitoring/Indexer.ps1
+++ AzureMonitoring/Indexer.ps1
@@ -1,4 +1,4 @@
 # =======> Indexer <=======
 # Script Author: Gurmukhnishan Singh (gurmukhnishansingh@gmail.com)
 # Script Version: 1.0
-# Purpose: This script will index Microsoft 365 data into elasticsearch.
+# Purpose: This script will index AWS log data into elasticsearch.
